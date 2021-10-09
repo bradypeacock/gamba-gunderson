9a5a04f7dc3a630804c0abbbc3aa9fbaa6faeafa
Brady Peacock <imbradyp98@gmail.com>
2021-10-09 16:18:35 -0400

    Make /coins not runnable in the middle of an action

--- commands/coins.js
+++ commands/coins.js
@@ -9,6 +9,15 @@ module.exports = {
 		.addUserOption(option => option.setName('member').setDescription('Member').setRequired(false)),
 	async execute(interaction)
 	{
+		if (await users.getPlaying(interaction.user.id) == 1)
+		{
+			return interaction.reply({ content: `:no_entry_sign: **${interaction.user.username}**, you are already in the middle of an action`, ephemeral: true });
+		}
+		else
+		{
+			await users.setPlaying(interaction.user.id, 1);
+		}
+
 		const currency_emoji = await game_info.get(1).emoji;
 
 		const target = await interaction.options.getUser('member') ?? interaction.user;
@@ -18,5 +27,7 @@ module.exports = {
 			.setColor('#0099ff')
 			.setDescription(`${target.username}#${target.discriminator} has ${coins} ${currency_emoji}`);
 		await interaction.reply({ embeds: [embed] });
+
+		await users.setPlaying(interaction.user.id, 0);
 	},
 };
